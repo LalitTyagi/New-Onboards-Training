23b49375e0a756a5eee375517301d7826656b783
Lalit Tyagi <luckytyagi61@gmail.com>
2018-06-11 17:10:42 +0530

    Update ProblemSet01.sql

--- Samples/ProblemSet01.sql
+++ Samples/ProblemSet01.sql
@@ -84,7 +84,14 @@ select count(*) from booking where date_from like '%SEP%';
 /*List the names and cities of guests who began a stay in New York in August.*/
 select Guest.Name,Guest.City from Guest join Booking on Guest.Guest_No=Booking.Guest_No where Booking.date_from like '%AUG%';
 
-/*List the hotel names and room numbers of any hotel rooms that have not been booked.*/
-/*List the hotel name and city of the hotel with the highest priced room.*/
-/*List hotel names, room numbers, cities, and prices for hotels that have rooms with prices lower than the lowest priced room in a Boston hotel.*/
-/*List the average price of a room grouped by city.*/
+/* List the hotel names and room numbers of any hotel rooms that have not been booked */ 
+select h.name,r.room_no from room r join hotel h on h.hotel_no=r.hotel_no where room_no not in (select room_no from booking); 
+ 
+/* List the hotel name and city of the hotel with the highest priced room */
+select h.name,h.city from hotel h join room r on  h.hotel_no=r.hotel_no where price in (select max(price) from room); 
+ 
+ /*  List hotel names, room numbers, cities, and prices for hotels that have rooms with prices lower than the lowest priced room in a Boston hotel */ 
+ select name ,city ,room_no ,price from hotel h join room r on r.hotel_no=h.hotel_no where price < (select min(price) from room r join hotel h on h.hotel_no=r.hotel_no where city='Boston'); 
+ 
+ /*  List the average price of a room grouped by city */ 
+ select city, avg(price) from room r join hotel h on h.hotel_no=r.hotel_no group by city; 
